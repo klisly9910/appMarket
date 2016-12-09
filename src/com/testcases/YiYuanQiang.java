e33b0ae81a230a6c7d0ea3e0401297fc70e30a8f
ftangxiaohua@163.com <ftangxiaohua@163.com>
2016-12-09 18:28:53 +0800

    update yiyuanqiang

--- src/com/testcases/YiYuanQiang.java
+++ src/com/testcases/YiYuanQiang.java
@@ -2,6 +2,8 @@ package com.testcases;
 
 import java.io.File;
 
+import android.R.integer;
+
 import com.android.uiautomator.core.UiCollection;
 import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObject;
@@ -10,6 +12,7 @@ import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 import com.pubMethod.Login;
 import com.pubMethod.Logout;
+import com.sun.jndi.ldap.Ber;
 
 public class YiYuanQiang extends UiAutomatorTestCase {
 	UiObject netError = new UiObject(
@@ -23,11 +26,19 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 		new UiObject(new UiSelector().text("机锋市场")).click();
 	}
 
+	/**
+	 * 从首页点击两次手机物理返回按钮退出机锋市场
+	 */
 	public void tearDown() throws UiObjectNotFoundException {
 		UiDevice.getInstance().pressBack();
 		UiDevice.getInstance().pressBack();
 	}
 
+	/**
+	 * 验证点：一元抢首页遍历可见的商品，点击检查商品详情页是否有网络错误 测试过程： -点击机锋市场首页一元抢--遍历首页并点击进入商品详情页
+	 * 
+	 * @throws UiObjectNotFoundException
+	 */
 	public void testHomeList() throws UiObjectNotFoundException {
 		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
 		yyq.click();
@@ -54,6 +65,12 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 
 	}
 
+	/**
+	 * 验证点：一元抢首页加入清单 测试过程： -进入一元抢首页-点击加入清单：- 已登录机锋市场：点击加入清单并截图保存
+	 * -未登录机锋市场：调用登录-登录后自动返回到一元抢首页
+	 * 
+	 * @throws UiObjectNotFoundException
+	 */
 	public void testJiaRuQingDan() throws UiObjectNotFoundException {
 		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
 		yyq.click();
@@ -69,7 +86,6 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 		if (signBtn.exists()) {
 			Login login = new Login();
 			login.login();
-
 			my.click();
 			Logout logout = new Logout();
 			logout.logout();
@@ -84,6 +100,12 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 
 	}
 
+	/**
+	 * 验证点：商品详情页未出现网络错误，参与记录显示 测试过程： -一元抢首页-点击一个商品-进入商品详情页-检查详情页是否出现网络错误
+	 * -已登录机锋市场：检查参与记录 -未登录机锋市场：调用登录方法登录
+	 * 
+	 * @throws UiObjectNotFoundException
+	 */
 	public void testProductDetail() throws UiObjectNotFoundException {
 		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
 		yyq.click();
@@ -118,6 +140,12 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 
 	}
 
+	/**
+	 * 验证点：揭晓页面未出现网络异常；遍历已开奖/未开奖，并分别点击进入商品详情页 测试过程： -点击揭晓-获取揭晓lists：
+	 * -遍历已开奖的商品信息-进入获奖者个人人中心-检查显示是否正确 -遍历未开奖的商品-点击进入详情页-检查未开奖状态显示是否正确
+	 * 
+	 * @throws UiObjectNotFoundException
+	 */
 	public void testJieXiao() throws UiObjectNotFoundException {
 		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
 		yyq.click();
@@ -174,6 +202,103 @@ public class YiYuanQiang extends UiAutomatorTestCase {
 			}
 
 		}
+		backImage.click();
+	}
+
+	/**
+	 * 【验证点】：清单未出现网络错误；清单数量显示；参与人数增减；编辑-删除 【测试过程】： -点击清单 -未登录：调用登录方法 ：已登录：
+	 * -有商品：商品数量显示正确；点击+/-后商品参数人数显示正确 -没有商品：提示信息显示正确
+	 * 
+	 * @throws UiObjectNotFoundException
+	 */
+	public void testQingDan() throws UiObjectNotFoundException {
+		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
+		yyq.click();
+		UiObject qingdan = new UiObject(new UiSelector().text("清单"));
+		qingdan.click();
+		UiObject signBtn = new UiObject(
+				new UiSelector().resourceId("com.mappn.gfan:id/tv_sign_in"));
+		UiCollection billList = new UiCollection(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_lv_list_view"));
+		UiObject itemCount = new UiObject(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_item_et_count"));
+		UiObject minus = new UiObject(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_item_iv_minus"));
+		UiObject plus = new UiObject(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_item_iv_plus"));
+		UiObject billNum = new UiObject(
+				new UiSelector().resourceId("com.mappn.gfan:id/bill_tv_num"));
+
+		if (signBtn.exists()) {
+			Login login = new Login();
+			login.login();
+			qingdan.click();
+		}
+		
+		if (billList.exists()) {
+			int billCount = billList.getChildCount(new UiSelector()
+					.resourceId("com.mappn.gfan:id/bill_ll_layout"));
+			String billNumTex = billNum.getText();
+			System.out.println(billNumTex.substring(1, 2));
+			// billNumTex.substring(1, 2);
+			assertEquals("商品件数", Integer.parseInt(billNumTex.substring(1, 2)),
+					billCount);
+			// assertTrue("商品件数", Integer.parseInt(billNumTex)==billCount);
+			int beforeClickPlus = Integer.parseInt(itemCount.getText());
+			plus.click();
+			int afterClickPlus = Integer.parseInt(itemCount.getText());
+			assertEquals("点击+号后", beforeClickPlus + 1, afterClickPlus);
+			minus.click();
+			int afterClickMinus = Integer.parseInt(itemCount.getText());
+			assertEquals("", afterClickMinus, afterClickPlus - 1);
+
+		} else if (!billList.exists()) {
+			UiObject noBillList = new UiObject(
+					new UiSelector()
+							.resourceId("com.mappn.gfan:id/empty_frame_layout"));
+			assertTrue(noBillList.exists());
+
+		}
+		Logout logout = new Logout();
+		logout.logout();
+		backImage.click();
+	}
+
+	public void testDeleteQingDan() throws UiObjectNotFoundException {
+		UiObject yyq = new UiObject(new UiSelector().text("一元抢"));
+		yyq.click();
+		UiObject qingdan = new UiObject(new UiSelector().text("清单"));
+		qingdan.click();
+		UiObject signBtn = new UiObject(
+				new UiSelector().resourceId("com.mappn.gfan:id/tv_sign_in"));
+		if (signBtn.exists()) {
+			Login login = new Login();
+			login.login();
+			qingdan.click();
+		}
+		
+		UiObject edit = new UiObject(
+				new UiSelector().resourceId("com.mappn.gfan:id/bill_tv_edit"));
+		edit.click();
+		UiDevice.getInstance().takeScreenshot(
+				new File("/sdcard/download/beforeDelete.png"));
+		UiObject checkBox = new UiObject(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_item_cb_check"));
+		checkBox.click();
+		UiObject deleteBtn = new UiObject(
+				new UiSelector()
+						.resourceId("com.mappn.gfan:id/bill_tv_delete_submit"));
+		deleteBtn.click();
+		UiObject positiveBtn = new UiObject(
+				new UiSelector().resourceId("com.mappn.gfan:id/btn_positive"));
+		positiveBtn.click();
+		UiDevice.getInstance().takeScreenshot(
+				new File("/sdcard/download/afterDelete.png"));
 		backImage.click();
 	}
 }
